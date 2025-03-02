d78a8b56acd2cfef6edf73314a2f93bfe13f4d5e
Luke Morris <clukemorris89@gmail.com>
2025-03-02 14:43:59 +0000

    Detekt fix

--- app/src/main/java/dev/rarebit/kollage/MainActivity.kt
+++ app/src/main/java/dev/rarebit/kollage/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
