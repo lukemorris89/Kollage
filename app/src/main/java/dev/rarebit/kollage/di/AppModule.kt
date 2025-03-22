385e98c0ade4692842ba2893b190d54477be1e72
Luke Morris <clukemorris89@gmail.com>
2025-03-22 12:26:09 +0000

    Add onbaording module

--- app/src/main/java/dev/rarebit/kollage/di/AppModule.kt
+++ app/src/main/java/dev/rarebit/kollage/di/AppModule.kt
@@ -1,11 +1,13 @@
 package dev.rarebit.kollage.di
 
 import dev.rarebit.core.di.coreModule
+import dev.rarebit.kollage.onboarding.di.onboardingModule
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
 val appModule = module {
     listOf(
         coreModule,
+        onboardingModule,
     ).let(::loadKoinModules)
 }
