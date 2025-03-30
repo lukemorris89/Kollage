6270a0cc4795175899cd89ba0d74c7587f25271d
Luke Morris <clukemorris89@gmail.com>
2025-03-30 10:17:45 +0100

    Add home nav

--- app/src/main/java/dev/rarebit/kollage/ui/home/HomeContent.kt
+++ app/src/main/java/dev/rarebit/kollage/ui/home/HomeContent.kt
@@ -1,11 +1,102 @@
 package dev.rarebit.kollage.ui.home
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import dev.rarebit.design.theme.Black
+import dev.rarebit.design.theme.KollageTheme
+import dev.rarebit.design.theme.White
+import dev.rarebit.design.theme.paddingLarge
+import dev.rarebit.design.theme.paddingSmall
+import dev.rarebit.kollage.ui.home.component.BottomNavBar
 import dev.rarebit.kollage.ui.home.data.HomeViewData
+import dev.rarebit.kollage.ui.home.data.NavigationItem
+import dev.rarebit.design.R as DR
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContent(
     viewData: HomeViewData,
     onViewAction: (HomeViewAction) -> Unit,
 ) {
+    val navigationItems = listOf(
+        NavigationItem.Gallery,
+        NavigationItem.More,
+    )
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Black),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Black,
+                    titleContentColor = White,
+                    navigationIconContentColor = White,
+                    actionIconContentColor = White,
+                ),
+                title = {},
+            )
+        },
+        bottomBar = {
+            BottomNavBar(
+                modifier = Modifier.padding(
+                    bottom = paddingLarge,
+                    start = paddingLarge,
+                    end = paddingLarge,
+                ),
+                selectedItem = viewData.selectedTab,
+                navigationItems = navigationItems,
+                onClickTab = { onViewAction(HomeViewAction.OnClickBottomNavigationTab(it)) },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(end = paddingSmall),
+                containerColor = White,
+                contentColor = Black,
+                shape = CircleShape,
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(id = DR.drawable.ic_add),
+                    contentDescription = null,
+                )
+            }
+        }
+    ) { contentPadding ->
+        Crossfade(
+            modifier = Modifier.padding(contentPadding),
+            targetState = viewData.selectedTab,
+        ) { selectedTab ->
+            when (selectedTab) {
+                NavigationItem.Gallery -> {}
+                NavigationItem.More -> {}
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeContentPreview() {
+    KollageTheme {
+        HomeContent(
+            viewData = HomeViewData(),
+            onViewAction = {}
+        )
+    }
 }
--- app/src/main/java/dev/rarebit/kollage/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/rarebit/kollage/ui/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package dev.rarebit.kollage.ui.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import dev.rarebit.kollage.ui.home.data.HomeViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -10,4 +13,29 @@ fun HomeScreen(
     navHostController: NavHostController,
     viewModel: HomeViewModel = koinViewModel()
 ) {
+    val viewData by viewModel.viewData.collectAsState()
+    HomeContent(
+        viewData = viewData,
+        onViewAction = {
+            with(viewModel) {
+                onViewAction(it)
+            }
+        },
+    )
+
+    LaunchedEffect(Unit) {
+        viewModel.viewEvent.collect { event ->
+            when (event) {
+                null -> Unit
+            }
+
+        }
+    }
+}
+
+context(HomeViewModel)
+private fun onViewAction(viewAction: HomeViewAction) {
+    when (viewAction) {
+        is HomeViewAction.OnClickBottomNavigationTab -> onClickBottomNavigationTab(viewAction.item)
+    }
 }
