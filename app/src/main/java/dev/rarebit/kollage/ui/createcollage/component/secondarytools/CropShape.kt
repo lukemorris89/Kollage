447f7cec28db542361492b7dcb413fa9ba0abba2
Luke Morris <clukemorris89@gmail.com>
2025-04-04 21:34:14 +0100

    Add crop util

--- app/src/main/java/dev/rarebit/kollage/ui/createcollage/component/secondarytools/CropShape.kt
+++ app/src/main/java/dev/rarebit/kollage/ui/createcollage/component/secondarytools/CropShape.kt
@@ -6,6 +6,6 @@ import dev.rarebit.design.R as DR
 enum class CropShape(
     @DrawableRes val iconRes: Int,
 ) {
-    SQUARE(iconRes = DR.drawable.ic_square),
+    RECTANGLE(iconRes = DR.drawable.ic_square),
     CIRCLE(iconRes = DR.drawable.ic_circle),
 }
